cf290404d89d78d8973621550ce4cd796527cf39
wangqj <wangqj@MacBookPro.lan>
2017-03-17 23:23:55 +0800

    optimize lcd

--- vdev/posix/src/posix_vdev_lcd.c
+++ vdev/posix/src/posix_vdev_lcd.c
@@ -10,12 +10,16 @@
 #define VLCD_X_SIZE 480
 #define VLCD_Y_SIZE 320
 
-SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
-static pthread_t RefreshThread;
-static BOOL RefreshFlag = 0;
-static pthread_mutex_t Mutex = PTHREAD_MUTEX_INITIALIZER;
-static uint16_t LcdBuffer[VLCD_Y_SIZE][VLCD_X_SIZE];
+typedef struct _lcd_info_t {
+    SDL_Window*     window;
+    SDL_Renderer*   renderer;
+    pthread_t       refresh_thread;
+    BOOL            refresh_flag;
+    pthread_mutex_t mutex;
+    uint16_t        buffer[VLCD_Y_SIZE][VLCD_X_SIZE];
+} lcd_info_t;
+
+static lcd_info_t *pLcdInfo = NULL;
 
 static void _posix_lcd_color16_to_24(uint16_t color, uint8_t *r, uint8_t *g, uint8_t *b)
 {
@@ -27,14 +31,14 @@ static void _posix_lcd_color16_to_24(uint16_t color, uint8_t *r, uint8_t *g, uin
 static void *_posix_lcd_task_refresh(void *args)
 {
     while (1) {
-        pthread_mutex_lock(&Mutex);
+        pthread_mutex_lock(&pLcdInfo->mutex);
 
-        if (RefreshFlag) {
-            SDL_RenderPresent(renderer);
-            RefreshFlag = FALSE;
+        if (pLcdInfo->refresh_flag) {
+            SDL_RenderPresent(pLcdInfo->renderer);
+            pLcdInfo->refresh_flag = FALSE;
         }
 
-        pthread_mutex_unlock(&Mutex);
+        pthread_mutex_unlock(&pLcdInfo->mutex);
         usleep(50000);
     }
 
@@ -46,16 +50,20 @@ static vdev_status_t posix_lcd_init(
 {
     int res;
 
+    pLcdInfo = (lcd_info_t *)malloc(sizeof(lcd_info_t));
+    VDEV_ASSERT_NOT_NULL(pLcdInfo);
+    memset(pLcdInfo, 0, sizeof(lcd_info_t));
+
     VDEV_ASSERT_SUCCESS(SDL_Init(SDL_INIT_VIDEO));
-    VDEV_ASSERT_SUCCESS(SDL_CreateWindowAndRenderer(VLCD_X_SIZE, VLCD_Y_SIZE, 0, &window, &renderer));
+    VDEV_ASSERT_SUCCESS(SDL_CreateWindowAndRenderer(VLCD_X_SIZE, VLCD_Y_SIZE, 0, &pLcdInfo->window, &pLcdInfo->renderer));
 
     /* create thread */
-    res = pthread_create(&RefreshThread, NULL, _posix_lcd_task_refresh, (void *)NULL);
+    res = pthread_create(&pLcdInfo->refresh_thread, NULL, _posix_lcd_task_refresh, (void *)NULL);
     if (0 != res) {
         return VDEV_STATUS_FAILURE;
     }
 
-    pthread_mutex_init(&Mutex, NULL);
+    pthread_mutex_init(&pLcdInfo->mutex, NULL);
 
     return VDEV_STATUS_SUCCESS;
 }
@@ -74,7 +82,7 @@ static vdev_status_t posix_lcd_fill_rect(
 
     for (y = ys; y <= ye; y++) {
         for (x = xs; x < xe; x++) {
-            LcdBuffer[y][x] = color;
+            pLcdInfo->buffer[y][x] = color;
         }
     }
 
@@ -84,13 +92,13 @@ static vdev_status_t posix_lcd_fill_rect(
     rect.w = xe - xs;
     rect.h = ye - ys;
 
-    pthread_mutex_lock(&Mutex);
+    pthread_mutex_lock(&pLcdInfo->mutex);
 
-    SDL_SetRenderDrawColor(renderer, r, g, b, SDL_ALPHA_OPAQUE);
-    SDL_RenderFillRect(renderer, &rect);
-    RefreshFlag = TRUE;
+    SDL_SetRenderDrawColor(pLcdInfo->renderer, r, g, b, SDL_ALPHA_OPAQUE);
+    SDL_RenderFillRect(pLcdInfo->renderer, &rect);
+    pLcdInfo->refresh_flag = TRUE;
 
-    pthread_mutex_unlock(&Mutex);
+    pthread_mutex_unlock(&pLcdInfo->mutex);
 
     return VDEV_STATUS_SUCCESS;
 }
@@ -102,16 +110,16 @@ static vdev_status_t posix_lcd_draw_point(
        _IN_ uint16_t color)
 {
     uint8_t r, g, b;
-    LcdBuffer[y][x] = color;
+    pLcdInfo->buffer[y][x] = color;
 
     _posix_lcd_color16_to_24(color, &r, &g, &b);
-    pthread_mutex_lock(&Mutex);
+    pthread_mutex_lock(&pLcdInfo->mutex);
 
-    SDL_SetRenderDrawColor(renderer, r, g, b, SDL_ALPHA_OPAQUE);
-    SDL_RenderDrawPoint(renderer, x, y);
-    RefreshFlag = TRUE;
+    SDL_SetRenderDrawColor(pLcdInfo->renderer, r, g, b, SDL_ALPHA_OPAQUE);
+    SDL_RenderDrawPoint(pLcdInfo->renderer, x, y);
+    pLcdInfo->refresh_flag = TRUE;
 
-    pthread_mutex_unlock(&Mutex);
+    pthread_mutex_unlock(&pLcdInfo->mutex);
 
     return VDEV_STATUS_SUCCESS;
 }
@@ -122,7 +130,7 @@ static vdev_status_t posix_lcd_get_point(
        _IN_ uint16_t y,
        _OUT_ uint16_t *color)
 {
-    *color = LcdBuffer[y][x];
+    *color = pLcdInfo->buffer[y][x];
 
     return VDEV_STATUS_SUCCESS;
 }
