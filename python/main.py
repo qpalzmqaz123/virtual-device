21984343bbc4669cfac1ffb8d9e4a787dec3ff04
qpalzmqaz123 <qpalzmqaz123@gmail.com>
2017-04-21 23:29:30 +0800

    update main.py

--- python/main.py
+++ python/main.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-from vdev import Vdev, Device
+from vdev import Vdev, Device, Enum2Py, config
 import devices
 
+
 if __name__ == '__main__':
+
+    model = Enum2Py(config.vdev_root_dir + '/inc/vdev_model.h', 'vdev_model_t')
+
     v = Vdev()
 
-    v.register_device(devices.Led, 3, 0)
-    v.register_device(devices.Stepmotor, 5, 0)
+    v.register_device(devices.Led, model.VDEV_MODEL_LED, 0)
+    v.register_device(devices.Stepmotor, model.VDEV_MODEL_STEPMOTOR, 0)
 
     v.start()
--- python/vdev/__init__.py
+++ python/vdev/__init__.py
@@ -1,3 +1,4 @@
 from .vdev import Vdev
 from .device import Device
 from .enum2py import Enum2Py
+from . import config
