511f5cb7bfdae36b1e6efbfb5d64c9823335d400
qpalzmqaz123 <qpalzmqaz123@gmail.com>
2017-05-01 21:16:43 +0800

    tmp

--- demo/main/demo_main.c
+++ demo/main/demo_main.c
@@ -9,8 +9,6 @@ static vdev_api_t Apis[] = {
     {VDEV_API_LED,       VDEV_MODEL_LED},
     {VDEV_API_OS,        VDEV_MODEL_OS},
     {VDEV_API_LCD,       VDEV_MODEL_LCD},
-    {VDEV_API_SDCARD,    VDEV_MODEL_SDCARD},
-    {VDEV_API_STEPMOTOR, VDEV_MODEL_STEPMOTOR}
 };
 
 
@@ -19,16 +17,13 @@ bsp_init(void)
 {
     vdev_os_api_t *p_os  = NULL;
     vdev_led_api_t *p_led = NULL;
-    vdev_stepmotor_api_t *p_stepmotor = NULL;
 
     vdev_api_init(Apis, sizeof(Apis) / sizeof(vdev_api_t));
     p_os = (vdev_os_api_t *)vdev_api_get(VDEV_API_OS);
     p_led = (vdev_led_api_t *)vdev_api_get(VDEV_API_LED);
-    p_stepmotor = (vdev_stepmotor_api_t *)vdev_api_get(VDEV_API_STEPMOTOR);
 
     p_os->init();
     p_led->init(0);
-    p_stepmotor->init(0);
     GUI_Init();
 }
 
@@ -52,15 +47,12 @@ task_led(void *arg)
 {
     vdev_os_api_t *p_os  = NULL;
     vdev_led_api_t *p_led = NULL;
-    vdev_stepmotor_api_t *p_stepmotor = NULL;
 
     p_os = (vdev_os_api_t *)vdev_api_get(VDEV_API_OS);
     p_led = (vdev_led_api_t *)vdev_api_get(VDEV_API_LED);
-    p_stepmotor = (vdev_stepmotor_api_t *)vdev_api_get(VDEV_API_STEPMOTOR);
 
     while (1) {
         p_os->msleep(500);
-        p_stepmotor->run(0);
         p_led->toggle(0); 
     }
 }
