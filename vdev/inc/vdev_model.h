bb728c3e02219a005df4c687183fc6a54fe6a6ee
wangqj <qpalzmqaz123@gmail.com>
2017-12-26 10:51:16 +0800

    optimize vdev_model.h

--- vdev/inc/vdev_model.h
+++ vdev/inc/vdev_model.h
@@ -2,15 +2,15 @@
 #define __VDEV_MODEL_H__
 
 typedef enum _vdev_model_t {
-    VDEV_MODEL_NULL = 0,
+    VDEV_MODEL_NULL         = 0,
 
-    VDEV_MODEL_GPIO,
-    VDEV_MODEL_LCD,
-    VDEV_MODEL_OS,
-    VDEV_MODEL_LED,
-    VDEV_MODEL_SDCARD,
-    VDEV_MODEL_STEPMOTOR,
-    VDEV_MODEL_FLOWMETER,
+    VDEV_MODEL_GPIO         = 1,
+    VDEV_MODEL_LCD          = 2,
+    VDEV_MODEL_OS           = 3,
+    VDEV_MODEL_LED          = 4,
+    VDEV_MODEL_SDCARD       = 5,
+    VDEV_MODEL_STEPMOTOR    = 6,
+    VDEV_MODEL_FLOWMETER    = 7,
 
     VDEV_MODEL_MAX
 } vdev_model_t;
